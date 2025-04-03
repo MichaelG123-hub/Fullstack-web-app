0b1b58b8451044f51f37b50f279b3a74eecc84ea
MichaelGastambides <Michaelgastambides@gmail.com>
2025-04-02 21:36:11 -0400

    created a fullstack

---
+++ client/app/page.jsx
@@ -0,0 +1,11 @@
+// build a component
+
+function Home(){
+    return(
+        <div>
+            <h1>Hello from next!</h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ server/index.js
@@ -0,0 +1,25 @@
+// This file is defining our express server and running it on port 3000
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+// middleware
+app.use(express.json()) // allows us to parse incoming JSON data from the client
+app.use(express.urlencoded()) // allows us to parse incoming URL Encoded data
+
+// add middleware for static files
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test/api', (req, res)=>{
+    res.send({
+        message: "Success! Server is operational."
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
