be532e02f1e91257e73b8d45753ce8bbc9dc4344
MichaelGastambides <Michaelgastambides@gmail.com>
2025-04-07 21:52:26 -0400

    connecting client and server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // This file is defining our express server and running it on port 3000
 
 const express = require('express')
+const path =  require('path')
 const app = express()
 const port = 3000
 
@@ -9,15 +10,17 @@ app.use(express.json()) // allows us to parse incoming JSON data from the client
 app.use(express.urlencoded()) // allows us to parse incoming URL Encoded data
 
 // add middleware for static files
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '../client/out'))) //make our client build folder public.
 
 app.get('/test/api', (req, res)=>{
     res.send({
         message: "Success! Server is operational."
     })
+})
+
+app.get('/', (req, res) =>{
+  res.sendFile(path.joint(__dirname, '../client/out', 'index.html'))
 })
 
 app.listen(port, () => {
